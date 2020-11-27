01d8164cbc8b2796495accaf2956f9b9b4fc5753
Jonas Vander Vennet <jonas.vdv@hotmail.com>
2020-11-27 14:19:34 +0100

    Create project structure

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# advent-of-code-2020
+# Advent Of Code 2020
+
+A Rust implementation of the 2020 edition of [Advent of Code](https://adventofcode.com/2020/).
+
+## Build
+
+```sh
+cargo build
+```
+
+## Test
+
+```sh
+cargo test
+```
+
+## Run
+
+### Run all
+
+```sh
+cargo run
+```
+
+### Run a specific day
+
+```sh
+cargo run -- --day 1
+```
+
+Thanks [MrRobb](https://github.com/MrRobb/advent-of-code-2019) for the clean project structure!
---
+++ src/example.rs
@@ -0,0 +1,94 @@
+// An example implementation of an AoC challenge
+// (source: aoc 2019 day 1 )
+
+use crate::util::read_lines;
+
+fn calculate_fuel(mass: usize) -> usize {
+    // can be checked with a checked_sub(2).unwrap_or(0) too!
+    let tmp = mass / 3;
+    if tmp <= 2 {
+        return 0;
+    }
+    tmp - 2
+}
+
+fn recurse_fuel(mass: usize) -> usize {
+    let fuel = calculate_fuel(mass);
+    if fuel <= 0 {
+        return 0;
+    }
+    fuel + recurse_fuel(fuel)
+}
+
+fn get_input() -> Result<Vec<usize>, std::io::Error> {
+    match read_lines("inputs/example.txt") {
+        Ok(input_str) =>
+        // https://stackoverflow.com/a/30608280
+        {
+            Ok(input_str
+                .map(|line| match line {
+                    Ok(s) => match s.parse() {
+                        Ok(num) => num,
+                        Err(_) => {
+                            println!("Could not convert line {} to usize", s);
+                            0
+                        }
+                    },
+                    Err(_) => {
+                        println!("Invalid line read from file");
+                        0
+                    }
+                })
+                .collect())
+        }
+        Err(e) => Err(e),
+    }
+}
+
+pub fn main() {
+	let input: Vec<usize> = match get_input() {
+		Ok(s) => s,
+		Err(e) => {eprintln!("ERR: {}", e); return}
+	};
+    // PART 1
+    let known_answer: usize = 3330521;
+    let part_1: usize = input.iter().map(|&i| calculate_fuel(i)).sum();
+    if part_1 != known_answer {print!("INCORRECT || ")}
+    println!("PART 1: {}", part_1);
+
+    // PART 2
+    let known_answer: usize = 4992931;
+    let part_2: usize = input.iter().map(|&i| recurse_fuel(i)).sum();
+    if part_2 != known_answer {print!("INCORRECT || ")}
+    println!("PART 2: {}", part_2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fuel_example_1() {
+        assert_eq!(calculate_fuel(12), 2);
+    }
+
+    #[test]
+    fn test_fuel_example_2() {
+        assert_eq!(calculate_fuel(14), 2);
+    }
+
+    #[test]
+    fn test_fuel_example_3() {
+        assert_eq!(calculate_fuel(1969), 654);
+    }
+
+    #[test]
+    fn test_fuel_example_4() {
+        assert_eq!(calculate_fuel(100756), 33583);
+    }
+
+    #[test]
+    fn test_fuel_example_5() {
+        assert_eq!(calculate_fuel(9992260), 3330521);
+    }
+}
